7ccb3d862a7a0aeafec76cc2256433cc379b867e
JosephDuvvani <josephduvvani@gmail.com>
2024-12-05 13:46:25 +0200

    Add a 'randomise' method to player object
    Each player can randomise their ship placement

--- src/player.js
+++ src/player.js
@@ -1,4 +1,5 @@
 import { Gameboard } from "./gameboard";
+import { checkCollision } from "./coordinates";
 
 export default () => {
   let gameBoard = Gameboard();
@@ -20,6 +21,34 @@ export default () => {
     placedShips.push(shipsIndex);
   };
 
+  const randomise = () => {
+    const length = gameBoard.getBoard().length;
+    for (let i = 0; i < ships.length; i++) {
+      let x = Math.floor(Math.random() * length);
+      let y = Math.floor(Math.random() * length);
+      if (checkCollision([x, y], gameBoard.getBoard())) {
+        i--;
+        continue;
+      }
+      let coordinates = [[x, y]];
+      let direction = Math.floor(Math.random() * 2);
+      for (let j = 1; j < ships[i]; j++) {
+        if (direction === 0) {
+          if (checkCollision([x + j, y], gameBoard.getBoard())) break;
+          coordinates.push([x + j, y]);
+        } else {
+          if (checkCollision([x, y + j], gameBoard.getBoard())) break;
+          coordinates.push([x, y + j]);
+        }
+      }
+      if (coordinates.length < ships[i]) {
+        i--;
+        continue;
+      }
+      gameBoard.placeShip(coordinates.splice(0, ships[i]));
+    }
+  };
+
   const attack = (coordinates) => gameBoard.receiveAttack(coordinates);
   const isDefeated = () => gameBoard.isAllSunk();
 
@@ -30,5 +59,6 @@ export default () => {
     sendShip,
     attack,
     isDefeated,
+    randomise,
   };
 };
